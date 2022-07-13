7b2822ab45e82eb396777bcb9d151e1691f33f90
refored <santiagopacini@gmail.com>
2022-07-13 17:48:30 -0300

    Protocol http converted to https

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,7 +1,7 @@
 export const useApi = async ubicacion => {
   try {
     let res = await fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=d41543a57e194e51b0701639220707&q=${ubicacion}`
+      `https://api.weatherapi.com/v1/forecast.json?key=d41543a57e194e51b0701639220707&q=${ubicacion}`
     );
     const data = await res.json();
     return data;
